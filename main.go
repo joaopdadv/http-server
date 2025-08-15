8ec5ee84641068b68deed42033de13a90e03d91c
João Pedro <joaopdadv@gmail.com>
2025-08-15 17:21:46 -0300

    concatena linhas lidas do arquivo

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -14,17 +15,28 @@ func main() {
 	}
 	defer file.Close() // defer -> quando a função main terminar, o arquivo será fechado
 
+	currentLine := ""
+
 	for {
 		data := make([]byte, 8)
 		_, err := file.Read(data)
 
 		if err == io.EOF {
+			fmt.Printf("read: %s\n", currentLine)
 			break
 		}
 		if err != nil {
 			log.Fatalf("Erro lendo arquivo: %v", err)
 		}
 
-		fmt.Printf("read: %s\n", data)
+		lineStrings := strings.Split(string(data), "\n")
+
+		currentLine += lineStrings[0]
+
+		if len(lineStrings) > 1 {
+			fmt.Printf("read: %s\n", currentLine)
+
+			currentLine = lineStrings[1]
+		}
 	}
 }
