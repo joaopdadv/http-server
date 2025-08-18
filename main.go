16cc04beb3bfac3b82c1793abdd582e20333d3dc
João Pedro <joaopdadv@gmail.com>
2025-08-17 21:07:54 -0300

    refactor de func main para funções auxiliares com goroutines e channels

--- main.go
+++ main.go
@@ -16,32 +16,44 @@ func main() {
 	}
 	defer file.Close() // defer -> quando a função main terminar, o arquivo será fechado
 
+	for i := range getLinesChannel(file) {
+		fmt.Printf("read: %s\n", i)
+	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
 	currentLine := ""
+	out := make(chan string)
 
-	for {
-		data := make([]byte, 8)
-		n, err := file.Read(data)
+	go func() {
+		defer close(out)
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
 
-		if err != nil {
-			if currentLine != "" {
-				fmt.Printf("read: %s\n", currentLine)
-			}
-			if errors.Is(err, io.EOF) {
+			if err != nil {
+				if currentLine != "" {
+					fmt.Printf("read: %s\n", currentLine)
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				log.Fatalf("Erro lendo arquivo: %v", err)
 				break
 			}
-			log.Fatalf("Erro lendo arquivo: %v", err)
-			break
+
+			lineStrings := strings.Split(string(data[:n]), "\n") // data[:n] é importante para não vazar bytes nulos ou lixo na string()
+
+			// Se tem apenas 1 item no vetor lineStrings, len(lineStrings)-1 = 0, ou seja, loop não roda pois 0 < 0 = false
+			for i := 0; i < len(lineStrings)-1; i++ {
+				out <- currentLine + lineStrings[i]
+				currentLine = ""
+			}
+
+			// coloca o último elemento sempre em currentLine
+			currentLine += lineStrings[len(lineStrings)-1]
 		}
+	}()
 
-		lineStrings := strings.Split(string(data[:n]), "\n") // data[:n] é importante para não vazar bytes nulos ou lixo na string()
-
-		// Se tem apenas 1 item no vetor lineStrings, len(lineStrings)-1 = 0, ou seja, loop não roda pois 0 < 0 = false
-		for i := 0; i < len(lineStrings)-1; i++ {
-			fmt.Printf("read: %s%s\n", currentLine, lineStrings[i])
-			currentLine = ""
-		}
-
-		// coloca o último elemento sempre em currentLine
-		currentLine += lineStrings[len(lineStrings)-1]
-	}
+	return out
 }
